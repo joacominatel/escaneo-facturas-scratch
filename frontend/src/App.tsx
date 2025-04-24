315c3620e70be02a026bbbce4b693e4326440764
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-04-24 11:27:10 -0300

    Feature: Add Toaster component for improved user notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import HomePage from "@/pages/HomePage"
+import { Toaster } from "sonner"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <main className="pt-4">
         <HomePage />
       </main>
+      <Toaster />
     </div>
   )
 }
