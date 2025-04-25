c185e2c46fb57198b2ea1b1c0ac86bb0ee78a848
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-04-25 20:30:27 -0300

    Deleted frontend folder to start from scratch

--- frontend/src/types/invoice.ts
+++
@@ -1,8 +0,0 @@
-// Define the possible invoice status types
-export type InvoiceStatus = 
-  | "processed" 
-  | "waiting_validation" 
-  | "processing" 
-  | "failed" 
-  | "rejected"
-  | "duplicated" // New status for duplicated invoices
