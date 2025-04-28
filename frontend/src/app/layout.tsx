7e152104392ea45b60dd59541bbf3e38f1edaf13
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-04-28 13:02:25 -0300

    Agregar el componente Toaster y establecer el tema predeterminado en el ThemeProvider, mejorando la gestión de notificaciones y la experiencia del usuario.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import "./globals.css"
 import { Navbar } from "@/components/navbar"
 import { ThemeProvider } from "@/contexts/theme-context"
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,13 +14,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider defaultTheme='light'>
           <div className="min-h-screen bg-background">
             <Navbar />
             <main className="mx-auto px-4 pt-24 pb-8">
               {children}
             </main>
           </div>
+        <Toaster />
         </ThemeProvider>
       </body>
     </html>
