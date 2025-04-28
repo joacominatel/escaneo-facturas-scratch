86e84944bbffe072fe913f3b2682bf4649267c7f
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-04-27 23:36:06 -0300

    Feature: Implementación de un archivo wsgi.py para la inicialización de la aplicación Flask y configuración de SocketIO, además de ajustes en docker-compose.yml y Dockerfile para mejorar la gestión de servicios y puertos.

--- app/core/config.py
+++ app/core/config.py
@@ -9,3 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PORT = int(os.getenv('PORT', 8010))
+    # Configuración para SocketIO y Celery
+    SOCKETIO_MESSAGE_QUEUE = os.getenv('SOCKETIO_MESSAGE_QUEUE', 'redis://localhost:6379/0')
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/1') # Asegúrate que coincida con tu config Celery
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/2') # Asegúrate que coincida
---
+++ wsgi.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde .env
+load_dotenv()
+
+from app import create_app
+from app.core.extensions import socketio
+
+# Crear la instancia de la aplicación Flask
+app = create_app()
+
+if __name__ == "__main__":
+    print("Iniciando servidor de desarrollo SocketIO...")
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv('PORT', 8010)), debug=(os.getenv('FLASK_DEBUG') == 'True')) 
