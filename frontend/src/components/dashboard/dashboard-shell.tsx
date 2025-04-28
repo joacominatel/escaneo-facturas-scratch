3ae2f0bbc81c394b1dfde694c2b0b445a55b2256
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-04-28 13:02:29 -0300

    Incorporar el ThemeProvider en el componente DashboardShell para establecer un tema predeterminado, mejorando la consistencia visual de la interfaz de usuario.

--- frontend/src/components/dashboard/dashboard-shell.tsx
+++ frontend/src/components/dashboard/dashboard-shell.tsx
@@ -1,12 +1,19 @@
-import type React from "react"
-import { cn } from "@/lib/utils"
+import type React from "react";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "../theme-provider";
 
 interface DashboardShellProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function DashboardShell({ children, className, ...props }: DashboardShellProps) {
+export function DashboardShell({
+  children,
+  className,
+  ...props
+}: DashboardShellProps) {
   return (
-    <div className={cn("grid items-start gap-8", className)} {...props}>
-      {children}
-    </div>
-  )
+    <ThemeProvider defaultTheme="light">
+      <div className={cn("grid items-start gap-8", className)} {...props}>
+        {children}
+      </div>
+    </ThemeProvider>
+  );
 }
