73aeabc06de5bbeb02bd20d7f2da7530569ad179
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-05-05 21:53:44 -0300

    fix lint

--- frontend/src/components/invoice-history/invoice-table/index.tsx
+++ frontend/src/components/invoice-history/invoice-table/index.tsx
@@ -36,7 +36,6 @@ export function InvoiceTable() {
         setRowSelection,
         setSearchTerm,
         setSelectedStatuses,
-        fetchData,
         openDetailsModal,
         resetFilters,
         setIsDetailModalOpen
